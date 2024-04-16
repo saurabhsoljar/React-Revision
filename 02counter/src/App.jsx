ef26999423acca7f701a4a145c7f12ea2cee4c56
saurabh kumar <saurabhslite2@gmail.com>
2024-04-16 18:57:04 +0530

    counter number project

---
+++ 02counter/src/App.jsx
@@ -0,0 +1,43 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+   
+
+
+  //let counter =15
+  
+  let [counter, setCounter] = useState(0);
+ 
+
+  const addValue = () =>{
+    //console.log("clicked", counter);
+    //counter = counter+ 1
+    setCounter(counter + 1)
+  }
+
+  const removeValue = () =>{
+    setCounter(counter - 1)
+  }
+
+   
+  return (
+    
+    <>
+      <h1>Chai aur react</h1>
+      <h2>Counter value: {counter}</h2>
+
+      <button 
+      onClick={addValue}
+      >Add value</button>
+      <br />
+      <button
+      onClick={removeValue}
+      >remove value</button>
+    </>
+  )
+}
+
+export default App

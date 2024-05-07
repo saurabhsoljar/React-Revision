1222f7811082a6c6e4a8805d86ae88726746609f
saurabh kumar <saurabhslite2@gmail.com>
2024-05-07 18:15:39 +0530

    add some components file

--- 07reactRouter/src/App.jsx
+++ 07reactRouter/src/App.jsx
@@ -1,13 +1,14 @@
 import { useState } from 'react'
 
 import './App.css'
+ 
 
 function App() {
   
 
   return (
     <>
-      <h1 className='bg-green-600 p-4'>saurabh </h1>
+      
     </>
   )
 }

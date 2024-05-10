2913324fdb5c5b6b1b40d273700c19972378a433
saurabh kumar <saurabhslite2@gmail.com>
2024-05-10 22:05:01 +0530

    context Api discussion

---
+++ 08minicontext/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import './App.css'
+import UserContextProvider from './Context/UserContextProvider'
+import Profile from './components/Profile'
+import Login from './components/Login'
+
+function App() {
+  return (
+    <UserContextProvider>
+      <h1>React with Chai and share is important</h1>
+      <Login/>
+      <Profile/>
+    </UserContextProvider>
+  )
+}
+
+export default App
